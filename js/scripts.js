4c89a69b7814b30a1ac88c1e4a802d8a4c87825a
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-03-26 10:35:26 -0700

    add questions/result statements to HTML

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,30 @@
-const language = $("input:radio[name=language]:checked").val();
+$(document).ready(function () {
+
+  const language = $("input:radio[name=language]:checked").val();
+
+  let languageTicker = 0; //want to use this keep adding then take this value into if/else to determine language
+
+
+  if (languageTicker <= 5) {
+    $('#youWantRuby').show();
+  } else if (languageTicker = 6 || languageTicker >= 10 || languageTicker < 15) {
+    $('#youWantPython').show();
+  } else {
+    $('#youWantRust').show();
+  }
+
+
+
+}
+
+
+
+
+/*if (age > 21) {
+   $('#drinks').show();
+ } else if (age === 21) {
+   alert("Now don't go wild!");
+   $('#drinks').show();
+ } else {
+   $('#under-21').show();
+ }*/
