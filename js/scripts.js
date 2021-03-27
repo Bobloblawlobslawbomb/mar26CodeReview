0eb48b88429aa818d55619464fd400bd6f8f31ab
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-03-26 22:34:58 -0700

    Update page with basic functionality

--- js/scripts.js
+++ js/scripts.js
@@ -1,19 +1,35 @@
-//business logic portion
 $(document).ready(function () {
+  //I want one of those yes no confirms to start
 
-  let languageTicker = 0; //want to use this keep adding then take this value into if/else to determine language
+  /*if (beginChoice ===true) {
+    // then do answerOne prompt
+  } else {
+    //"say why are you here then?" --WIP:hidden quest
+  }*/
 
-  const languageChoice = languageTicker;
+  const answerOne = prompt("Which programming language would you like to find in your shoe?")
 
-  if (languageChoice <= 5) {
+  const answerTwo = prompt("Which programming language would you most like to eat?")
+
+  const answerThree = prompt("Which programming language would you LEAST mind stepping on?")
+
+  const answerFour = prompt("Which programming language would you MOST like to step on?")
+
+  const answerFive = prompt("Which programming language would you confront in a dark alley?")
+
+  const allThemAnswers = answerOne + answerTwo + answerThree + answerFour + answerFive
+
+  const dumbAnswer = allThemAnswers.length
+  const evenDumberAnswer = (dumbAnswer / 10) + 1
+
+  alert(dumbAnswer)//for testing variable
+  alert(evenDumberAnswer)//for testing variable
+
+  if (evenDumberAnswer <= 3) {
     $('#youWantRuby').show();
-  } else if (languageChoice <= 14) {
+  } else if (evenDumberAnswer <= 5) {
     $('#youWantPython').show();
   } else {
     $('#youWantRust').show();
   }
-
-  //user input logic section
-
-  const language = $("input:radio[name=language]:checked").val();
 })
