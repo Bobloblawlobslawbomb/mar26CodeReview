adabc784d7b2ef85d1e0bef00d04ba8f4f996db8
Giancarlo Vigneri <bobloblaw.vigneri@gmail.com>
2021-04-01 13:44:56 -0700

    add all working funtionality except grabbing a value from radio button

--- js/scripts.js
+++ js/scripts.js
@@ -1,17 +1,17 @@
-let answerOne //went back and forth on const or let. const because they dont change 
-let answerTwo //but let because Im declaring them'early' --0let seems to not throw an error tho...
-let answerThree
-let answerFour
-let answerFive
-const answerSum = answerOne + answerTwo + answerThree + answerFour + answerFive
+//let answerOne //went back and forth on const or let. const because they dont change 
+//let answerTwo //but let because Im declaring them'early' --let seems to not throw an error tho...
+//let answerThree
+//let answerFour
+//let answerFive
+//const answerSum = answerOne + answerTwo + answerThree + answerFour + answerFive
 
-if (answerSum <= 5) {
-  $('#youWantPython').show();
-} else if (answerSum <= 10) {
-  $('#youWantRuby').show();
-} else {
-  $('#youWantRust').show();
-}
+//if (answerSum <= 5) {
+// $('#youWantPython').show();
+//} else if (answerSum <= 10) {
+// $('#youWantRuby').show();
+//} else {
+// $('#youWantRust').show();
+//}
 
 $(document).ready(function () {
 
@@ -22,48 +22,68 @@ $(document).ready(function () {
   }
   else {
     $("h1").text("That's cool. You do you.")
-  }  //this works
+  }  //this works!!!
+
+  let answerOne //went back and forth on const or let. const because they dont change 
+  let answerTwo //but let because Im declaring them'early' --let seems to not throw an error tho...
+  let answerThree
+  let answerFour
+  let answerFive
+  //let answerSum = answerOne + answerTwo + answerThree + answerFour + answerFive
 
 
   $("#formOne").submit(function (event) {
     event.preventDefault();
-    answerOne = $("value#formOne").parseInt()// is $('value#formOne') correct?
-    $("#formTwo").show()
-    $("#formOne").hide()
-    //I need to make something like: const flavor = $("input:radio[name=flave]:checked").val();
+    let answerOne = $("input:language :checked").val();// is $('value#formOne') correct? added let afternoon 4/1 SO CLOSE. JUST HAVE TO TWEAK INPUT
+    $("#formTwo").slideDown()
+    $("#formOne").slideUp()
+    console.log(answerOne)
   })
+  console.log(answerOne)
 
-  //answerOne = $("#formOne").parseInt(value) closer?
+  //I need to make something like: const flavor = $("input:radio[name=flave]:checked").val();
+  //this seems to work but i am having scope/variable issues
+
+  //answerOne = $("value#formOne").val() closer?
   // I think my issue here has to do with submit/input:radio/and something with the value, but need guidance
 
   $("#formTwo").submit(function (event) {
     event.preventDefault();
-    answerTwo = $("value#questionTwo").parseInt()
+    answerTwo = $("value#formTwo").val()
     $("#formThree").slideDown()
     $("#formTwo").slideUp()
   })
   $("#formThree").submit(function (event) {
     event.preventDefault();
-    answerThree = $("value#questionThree").parseInt()
+    answerThree = $("value#formThree").val()
     $("#formFour").slideDown()
     $("#formThree").slideUp()
   })
   $("#formFour").submit(function (event) {
     event.preventDefault();
-    answerFour = $("value#questionFour").parseInt()
+    answerFour = $("value#formFour").val()
     $("#formFive").slideDown()
     $("#formFour").slideUp()
   })
   $("#formFive").submit(function (event) {
     event.preventDefault();
-    answerFive = $("value#questionFive").parseInt()
+    answerFive = $("value#formFive").val()
     $("#formFive").slideUp()
     $("#results").fadeIn()
-    $(h1).hide()
+    $("h1").hide()
 
   })
 
-  //all of this is to grab values of the questions and use $ to show/hide question forms
+  let answerSum = answerOne + answerTwo + answerThree + answerFour + answerFive
+
+  if (answerSum <= 5) {
+    $('#youWantPython').show();
+  } else if (answerSum <= 10) {
+    $('#youWantRuby').show();
+  } else {
+    $('#youWantRust').show();
+  }
+  //all of this is to grab values of the questions and use $ to show/hide question forms --it seems to work but I am having scope issues
 
 
   //add lines of code to: 
@@ -76,5 +96,9 @@ $(document).ready(function () {
 
   $(".refresh").click(function () {
     location.reload()
-  })//WIP also need guidance here...
+  })//WIP also need guidance here... I think is works tho
+
+
+
+
 })
